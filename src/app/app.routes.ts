f54af7b4cef9dfc043036f7dc676dd48d46d31a1
luizgcl <luiizgcl@gmail.com>
2025-04-24 19:35:29 -0300

    chore: setup routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,33 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { PhoneFormComponent } from './pages/phone-form/phone-form.component';
+import { PhoneManagementComponent } from './pages/phone-management/phone-management.component';
 
 export const routes: Routes = [
   {
     path: '',
     children: [
-      { path: '', redirectTo: 'home', pathMatch: 'full' },
-      { path: 'home', component: HomeComponent },
-    ]
-  }
+      { path: '', redirectTo: 'inicio', pathMatch: 'full' },
+      {
+        path: 'inicio',
+        component: HomeComponent,
+        title: 'Gerencia Fone | Início',
+      },
+      {
+        path: 'numeros-disponiveis',
+        component: PhoneManagementComponent,
+        title: 'Gerencia Fone | Números disponíveis',
+      },
+      {
+        path: 'novo-numero',
+        component: PhoneFormComponent,
+        title: 'Gerencia Fone | Novo número',
+      },
+      {
+        path: 'editar-numero/:id',
+        component: PhoneFormComponent,
+        title: 'Gerencia Fone | Editar número',
+      },
+    ],
+  },
 ];
