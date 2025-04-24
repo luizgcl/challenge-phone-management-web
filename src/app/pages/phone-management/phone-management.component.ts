2377af02b65c4e578923c7c3c1962f5d57816ac6
luizgcl <luiizgcl@gmail.com>
2025-04-24 19:30:54 -0300

    feat(phone-management): create phone-management page

---
+++ src/app/pages/phone-management/phone-management.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-phone-management',
+  standalone: true,
+  imports: [],
+  templateUrl: './phone-management.component.html',
+  styleUrl: './phone-management.component.css'
+})
+export class PhoneManagementComponent {
+
+}
