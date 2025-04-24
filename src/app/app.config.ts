a5eb6eb72add602f691ab01b3c7a4ecc9398dd3e
luizgcl <luiizgcl@gmail.com>
2025-04-23 22:46:58 -0300

    chore: changes in config to accept http requests

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,19 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { routes } from './app.routes';
+import { httpInterceptor } from './http.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideHttpClient(
+      withFetch(),
+      withInterceptors([
+        httpInterceptor,
+      ])
+    ),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes)
+  ]
 };
