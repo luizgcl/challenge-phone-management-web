d6ef9d38fe10a6ce66aa3a68830be315ad8c6fe9
luizgcl <luiizgcl@gmail.com>
2025-04-24 21:31:35 -0300

    chore: configure currency

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,19 +1,29 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  DEFAULT_CURRENCY_CODE,
+  LOCALE_ID,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
-import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import { registerLocaleData } from '@angular/common';
+import {
+  provideHttpClient,
+  withFetch,
+  withInterceptors,
+} from '@angular/common/http';
+import ptBr from '@angular/common/locales/pt';
 import { routes } from './app.routes';
 import { httpInterceptor } from './http.interceptor';
 
+registerLocaleData(ptBr);
+
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideHttpClient(
-      withFetch(),
-      withInterceptors([
-        httpInterceptor,
-      ])
-    ),
+    { provide: LOCALE_ID, useValue: 'pt' },
+    { provide: DEFAULT_CURRENCY_CODE, useValue: 'BRL' },
+    provideHttpClient(withFetch(), withInterceptors([httpInterceptor])),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+  ],
 };
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,23 @@
-import { AsyncPipe, CommonModule } from '@angular/common';
+import { AsyncPipe, CommonModule, CurrencyPipe } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { LogoComponent } from './logo/logo.component';
 
 @NgModule({
   declarations: [LogoComponent],
-  imports: [CommonModule, FormsModule, ReactiveFormsModule, AsyncPipe],
-  exports: [LogoComponent, FormsModule, ReactiveFormsModule, AsyncPipe],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AsyncPipe,
+    CurrencyPipe,
+  ],
+  exports: [
+    LogoComponent,
+    FormsModule,
+    ReactiveFormsModule,
+    AsyncPipe,
+    CurrencyPipe,
+  ],
 })
 export class SharedModule {}
